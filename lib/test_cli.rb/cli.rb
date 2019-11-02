d001d3d2ea0f0617070a95237a5c002d1a3e18cd
Toni-Ty <antonique2479@yahoo.com>
2019-11-02 01:27:27 +0000

    updated summary and description

---
+++ lib/test_cli.rb/cli.rb
@@ -0,0 +1,12 @@
+# module VirtualCafe
+# #   class Error < StandardError; end
+# #   # Your code goes here...
+# end
+
+require 'pry'
+require 'colorize'
+
+require_relative "./test_cli/version"
+# require_relative './test_cli/cli_interface'
+# require_relative './test_cli/virtual_cafe_drinks'
+# require_relative './test-cli/cli_scraper'
