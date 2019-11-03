df2e05fa8d9dfebf8615d46ed99e12deb4daa498
Toni-Ty <antonique2479@yahoo.com>
2019-11-03 07:52:28 +0000

    updated cli_interface methods

--- lib/cli_application/cli_interface.rb
+++ lib/cli_application/cli_interface.rb
@@ -12,42 +12,36 @@ class GoogleLib::CLI_INTERFACE
         show_authors
         show_books
         show_publisher
-    end
+  end
 
 
   #gets user input in the form of a category
   def user_input
     @input = gets.strip
-       @input ==  GoogleBooks::API.search(@input)
-    #     show_authors
-    #     show_books
-    #     show_publisher
-    #   else
-    #     puts "please enter a relevant category"
-    # end
+    @input ==  GoogleBooks::API.search(@input)
   end
 
   # returns book authors
   def show_authors
     books = GoogleBooks::API.search(@input, :count => 5)
     books.each do |book|
-      puts book.authors
-      end
+    puts book.authors
     end
+  end
 
   #returns book titles
   def show_books
     books = GoogleBooks::API.search(@input, :count => 5)
     books.each do |book|
-      puts book.title
-      end
+    puts book.title
     end
+  end
 
     #returns book publishers
     def show_publisher
       books = GoogleBooks::API.search(@input, :count => 5)
       books.each do |book|
-        puts book.publisher
+      puts book.publisher
     end
   end
 
