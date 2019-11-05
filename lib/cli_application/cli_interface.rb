d579c90fb91b53de7858b8d28a816c4cf8cafb6a
Toni-Ty <antonique2479@yahoo.com>
2019-11-05 08:57:21 +0000

    created new methods

--- lib/cli_application/cli_interface.rb
+++ lib/cli_application/cli_interface.rb
@@ -9,46 +9,69 @@ class GoogleLib::CLI_INTERFACE
     puts "Hello!  Welcome to Google Library, please enter a book topic" .blue
     puts "      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  ".blue
         user_input
-        show_authors
-        show_books
-        show_publisher
+        get_books
+        show_all_books
+        #  get user input for book that they want (ie. "2")
+        #  save that book to their reading list
+
+
+        # show_authors
+        # show_books
+        # show_publisher
         save_book
   end
-
 
   #gets user input in the form of a category
   def user_input
     @input = gets.strip
-    @input ==  GoogleBooks::API.search(@input)
   end
+
+  #loads google library
+  def get_books
+    @books =  GoogleBooks::API.search(@input, :count => 5)
+  end
+
+    def show_all_books
+      count = 1
+      @books.each do |book|
+      puts "#{count}. #{book.title}, #{book.authors}, #{book.isbn}"
+      count += 1
+      end
+    end
 
   # returns book authors
   def show_authors
-    books = GoogleBooks::API.search(@input, :count => 5)
-    books.each do |book|
+    # books = GoogleBooks::API.search(@input, :count => 5)
+    @books.each do |book|
     puts book.authors
     end
   end
 
   #returns book titles
   def show_books
-    books = GoogleBooks::API.search(@input, :count => 5)
-    books.each do |book|
+    @books.each do |book|
     puts book.title
     end
   end
 
     #returns book publishers
   def show_publisher
-    books = GoogleBooks::API.search(@input, :count => 5)
-    books.each do |book|
+    @books.each do |book|
     puts book.publisher
     end
   end
 
   #save book
   def save_book
-    puts "If you would like to save a book to your reading list, please type the name of the book." .blue
+    puts "If you would like to save a book to your reading list, please type the number of the book you'd like to save to your reading list."
+    # grab the user input "2
+    book_input = gets.strip # book_input is the reading list book id
+    book_to_save = @books.find{|book| book.isbn == book_input }
+    binding.pry
+    # book_to_save =  Google_library::Readinglist.find_by_id()
+    # book (that user wants to save) = @books[1]
+    # @@reading_list << book
+    # puts "reading list is #{@@reading_list}"
   end
 
 end
