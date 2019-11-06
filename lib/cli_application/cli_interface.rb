baab4e017454635e95d935a466cbf32f279ed6e0
Toni-Ty <antonique2479@yahoo.com>
2019-11-06 04:47:42 +0000

    modified method names

--- lib/cli_application/cli_interface.rb
+++ lib/cli_application/cli_interface.rb
@@ -1,28 +1,14 @@
-# CLI Controller
-#  User Interface and Meu
-class GoogleLib::CLI_INTERFACE
-#   @@reading_list = []
-#
-#   def initialize
-#     @@reading_list << self
-# end
-#
-
-
+class GoogleLib::CLI_INTERFACE #  User Interface and Meu
 
   def call
-    puts "Hello!  Welcome to Google Library, please enter a book topic" .blue
+    puts "Hello!  Welcome to Google Lib, please enter a book topic" .blue
     puts "      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  ".blue
         user_input
         get_books
         show_all_books
         get_user_selection
-        print_reading_list
+        reading_list_options
   end
-
-  # def self.all
-  #   @@reading_list
-  # end
 
   #gets user input in the form of a category
   def user_input
@@ -34,57 +20,38 @@ class GoogleLib::CLI_INTERFACE
     @books =  GoogleBooks::API.search(@input, :count => 5)
   end
 
-  #returns user input of book selection
+  #returns user book selection
   def show_all_books
     @books.each  { |book| puts "#{book.title}, #{book.authors}, #{book.publisher}"}
   end
 end
 
-  #gets book title from user
+  #gets book titles from user
     def get_user_selection
-      # @push_books = []
       puts "If you would like to save a book to your reading list, please type the title of the book you'd like to save." .blue
       book_input = gets.strip
-      # saved_book = @books.find {|book| book.title == book_input }
       @books.each do |book|
         if book.title == book_input
-         saved_book = book_input
-         @all_the_books = GoogleLib::Google_library.all
-         @all_the_books << saved_book
-        #  @push_books << saved_book
+          saved_book = book_input
+          @all_the_books = GoogleLib::Google_library.all
+          @all_the_books << saved_book
       end
-       end
-
-end
-  def invalid_response
-    puts "Sorry that's an invalid response. Please try again" .blue
-    puts ""
-    call
+    end
   end
 
-  #prints reading list
-  def print_reading_list
+  #option for additional books, reading list, and exit
+  def reading_list_options
     # puts @push_books
     puts "Would you like to enter a new book topic to search?" .blue
-    puts "If so, please type [Y]...Or type [exit] to leave" .blue
+    puts "If so, please type [Y]...Or type [see list] for your reading list...Or type [exit] to leave" .blue
     user_input
-		if @input == "Y"
-			call
-    elsif @input == "see books"
-      puts @all_the_books
-
-		# else @input == "exit"
-    else
-			puts "Have a great day!".magenta
-			exit
-				end
+		  if @input == "Y"
+			  call
+      elsif @input == "see list"
+        puts @all_the_books
+        call
+      else
+  			puts "Have a great day!".magenta
+  			exit
 			end
-
-
-# end
-# end
-    # binding.pry
-
-#   def reset_reading_list
-#     @push_books.clear
-#   end
+		end
--- lib/cli_application/reading_library.rb
+++ lib/cli_application/reading_library.rb
@@ -1,49 +1,14 @@
 class GoogleLib::Google_library
-#
-#   attr_accessor :authors, :titles, :publisher
-#
-#   @@all_books = []
-#
-#
-#   def initialize
-#     @authors = authors
-#     @titles = titles
-#     @publishers = publisher
-#     @@all_books << self
-#   end
-#
-#   #all saved books
-#   def self.all
-#     @@all_books
-#   end
-#
-#
-# end
 
 
-    @@count = 0
-    @@instances = []
+
+    @@reading_list = []
 
     def initialize(options)
-           @@count += 1
-           @@instances << self
+           @@reading_list << self
     end
 
     def self.all
-        @@instances
+        @@reading_list
     end
-
-    def self.count
-        @@count
-    end
-
-#     def self.count
-#         ObjectSpace.each_object(self).count
-#     end
-#
-#
-#     def self.all
-#         ObjectSpace.each_object(self).to_a
-#     end
-# end
 end
