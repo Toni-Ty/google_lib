bf85414d88cf75678810ce49297cb53e343ee7e6
Toni-Ty <antonique2479@yahoo.com>
2019-11-13 05:02:29 +0000

    updated method names

--- lib/cli_application/cli_interface.rb
+++ lib/cli_application/cli_interface.rb
@@ -1,33 +1,35 @@
-class GoogleLib::CLI_INTERFACE #User Interface and Menu
+class GoogleLib::CLI_INTERFACE
 
   def call
     puts "Hello!  Welcome to Google Lib, please enter a book topic" .blue
     puts "      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  " .blue
-        user_input
-        get_books
+        get_user_input
+        search_google_books
         show_all_books
-        get_user_selection
+        show_user_selection
+        save_book_title
         reading_list_options
   end
 
-  #gets user input
-  def user_input
+  def get_user_input
     @input = gets.strip
   end
 
-  #loads google library
-  def get_books
+  def search_google_books
+  # def get_books
     @books =  GoogleBooks::API.search(@input, :count => 5)
   end
 
   #returns user book selection
-  def show_all_books
+  # def show_all_books
+  def show_user_selection
     @books.each  { |book| puts "#{book.title}, #{book.authors}, #{book.publisher}"}
     end
   end
 
   #gets book titles from user
-  def get_user_selection
+  # def get_user_selection
+  def save_book_title
     puts "If you would like to save a book to your reading list, please type the title of the book you'd like to save." .blue
     book_input = gets.strip
     @books.each do |book|
@@ -43,7 +45,7 @@ class GoogleLib::CLI_INTERFACE #User Interface and Menu
   def reading_list_options
     puts "Would you like to enter a new book topic to search?" .blue
     puts "If so, please type [Y]...Or type [see list] for your reading list...Or type [exit] to leave" .blue
-    user_input
+    gets_user_input
 		  if @input == "Y"
 			  call
       elsif @input == "see list"
